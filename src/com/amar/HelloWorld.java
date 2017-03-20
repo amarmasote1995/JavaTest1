09f0804bb589b5e21195215e4f87afaaa80e317e
amarmasote1995 <mrpandu200@gmail.com>
2017-03-20 15:56:48 +0530

    setProperty - user.name

--- src/com/amar/HelloWorld.java
+++ src/com/amar/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World ");
 		
-	// Hello
+		
+		System.setProperty("user.name", "Amar");
+		System.out.println(System.getProperty("user.name"));
 
 	}
 
