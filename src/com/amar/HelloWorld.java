8ada21cf26f3ac9fb0289521cb45a26fa16a74a7
amarmasote1995 <mrpandu200@gmail.com>
2017-03-20 15:15:40 +0530

    minor changes
    changes

--- src/com/amar/HelloWorld.java
+++ src/com/amar/HelloWorld.java
@@ -5,8 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World ");
 		
-		
-		// System.out.println(System.getProperties());
+	// Hello
 
 	}
 
